249815765770c0e4b57a5eec6545a64dc6c55613
Faeez Shaikh <faeezshaikh@Faeezs-MacBook-Pro.local>
2016-06-24 00:48:48 -0500

    cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -1,143 +1,106 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module(
-		'starter',
-		[ 'ionic', 'starter.controllers', 'auth0', 'angular-storage',
-				'angular-jwt','ngCordova' ,'firebase'])
+  'starter', ['ionic', 'starter.controllers', 'auth0', 'angular-storage',
+    'angular-jwt', 'ngCordova', 'firebase'
+  ])
 
-.run(function($ionicPlatform,auth,$rootScope,store,jwtHelper) {
-	$ionicPlatform.ready(function() {
-		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-		// for form inputs)
-		if (window.cordova && window.cordova.plugins.Keyboard) {
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-			cordova.plugins.Keyboard.disableScroll(true);
+.run(function($ionicPlatform, auth, $rootScope, store, jwtHelper) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-		}
-		if (window.StatusBar) {
-			// org.apache.cordova.statusbar required
-			StatusBar.styleDefault();
-		}
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
 
-	});
-	
-	//This hooks all auth avents
-	  auth.hookEvents();
-	  //This event gets triggered on URL change
-	  var refreshingToken = null;
-	  $rootScope.$on('$locationChangeStart', function() {
-	    var token = store.get('token');
-	    var refreshToken = store.get('refreshToken');
-	    if (token) {
-	      if (!jwtHelper.isTokenExpired(token)) {
-	        if (!auth.isAuthenticated) {
-	          auth.authenticate(store.get('profile'), token);
-	        }
-	      } else {
-	        if (refreshToken) {
-	          if (refreshingToken === null) {
-	            refreshingToken = auth.refreshIdToken(refreshToken).then(function(idToken) {
-	              store.set('token', idToken);
-	              auth.authenticate(store.get('profile'), idToken);
-	            }).finally(function() {
-	              refreshingToken = null;
-	            });
-	          }
-	          return refreshingToken;
-	        } else {
-	          $location.path('/login');// Notice: this url must be the one defined 
-	        }                          // in your login state. Refer to step 5.
-	      }
-	    }
-	  });
-	  
-	  
-	  
+  });
+
 })
 
-.factory('PersonService', function($http){
-	var BASE_URL = "http://api.randomuser.me/";
-	var items = [];
-	
-	function shuffle(array) {
-		  var currentIndex = array.length, temporaryValue, randomIndex ;
+.factory('PersonService', function($http) {
+  var BASE_URL = "http://api.randomuser.me/";
+  var items = [];
 
-		  // While there remain elements to shuffle...
-		  while (0 !== currentIndex) {
+  function shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue, randomIndex;
 
-		    // Pick a remaining element...
-		    randomIndex = Math.floor(Math.random() * currentIndex);
-		    currentIndex -= 1;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-		    // And swap it with the current element.
-		    temporaryValue = array[currentIndex];
-		    array[currentIndex] = array[randomIndex];
-		    array[randomIndex] = temporaryValue;
-		  }
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-		  return array;
-		}
-	
-	return {
-		GetFeed: function(){
-			
-			return $http({
-//			    url: BASE_URL+'?results=10',
-				url: 'data/data.json',
-			    method: "GET",
-			    
-			    headers: {
-			        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-			    }
-			}).then(function(response){
-				items = response.data.results;
-				return shuffle(items);
-			});
-//			return $http.get(BASE_URL+'?results=10').then(function(response){
-//				items = response.data.results;
-//				return items;
-//			});
-		},
-		GetNewUsers: function(){
-//			return $http.get(BASE_URL+'?results=2').then(function(response){
-//				items = response.data.results;
-//				return items;
-//			});
-			return $http({
-			    url: 'data/data.json',
-			    method: "GET",
-			    
-			    headers: {
-			        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-			    }
-			}).then(function(response){
-				items = response.data.results;
-				return shuffle(items);
-			});
-		},  
-		GetOldUsers: function(){
-//			return $http.get(BASE_URL+'?results=10').then(function(response){
-//				items = response.data.results;
-//				return items;
-//			});
-			return $http({
-//			    url: BASE_URL+'?results10',
-				url: 'data/data.json',
-			    method: "GET",
-			    
-			    headers: {
-			        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-			    }
-			}).then(function(response){
-				items = response.data.results;
-				return shuffle(items);
-			});
-		}
-	}
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  return {
+    GetFeed: function() {
+
+      return $http({
+        //			    url: BASE_URL+'?results=10',
+        url: 'data/data.json',
+        method: "GET",
+
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+      }).then(function(response) {
+        items = response.data.results;
+        return shuffle(items);
+      });
+      //			return $http.get(BASE_URL+'?results=10').then(function(response){
+      //				items = response.data.results;
+      //				return items;
+      //			});
+    },
+    GetNewUsers: function() {
+      //			return $http.get(BASE_URL+'?results=2').then(function(response){
+      //				items = response.data.results;
+      //				return items;
+      //			});
+      return $http({
+        url: 'data/data.json',
+        method: "GET",
+
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+      }).then(function(response) {
+        items = response.data.results;
+        return shuffle(items);
+      });
+    },
+    GetOldUsers: function() {
+      //			return $http.get(BASE_URL+'?results=10').then(function(response){
+      //				items = response.data.results;
+      //				return items;
+      //			});
+      return $http({
+        //			    url: BASE_URL+'?results10',
+        url: 'data/data.json',
+        method: "GET",
+
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+      }).then(function(response) {
+        items = response.data.results;
+        return shuffle(items);
+      });
+    }
+  }
 })
 
 // This is for one on one chat..
@@ -163,124 +126,66 @@ angular.module(
 //})
 
 .config(
-		function($stateProvider, $urlRouterProvider, authProvider,
-				jwtInterceptorProvider, $httpProvider) {
-			
-			$stateProvider
+    function($stateProvider, $urlRouterProvider, authProvider,
+      jwtInterceptorProvider, $httpProvider) {
 
-			.state('login', {
-				url : "/login",
-				templateUrl : "templates/login.html",
-				controller : 'LoginCtrl'
-			})
+      $stateProvider
 
-			.state('app', {
-				url : '/app',
-				abstract : true,
-				templateUrl : 'templates/menu.html',
-				controller : 'AppCtrl',
-				data : {
-					requiresLogin : true
-				}
-			})
 
-			.state('app.chat', {
-				url : '/chat',
-				views : {
-					'menuContent' : {
-						templateUrl : 'templates/chat.html'
-					}
-				}
-			})
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl',
+        data: {
+          requiresLogin: true
+        }
+      })
 
-			.state('app.browse', {
-				url : '/browse',
-				views : {
-					'menuContent' : {
-						templateUrl : 'templates/browse.html',
-						controller : 'PlaylistsCtrl'
-					}
-				}
-			}).state('app.playlists', {
-				url : '/playlists',
-				views : {
-					'menuContent' : {
-						templateUrl : 'templates/playlists.html',
-						controller : 'PlaylistsCtrl'
-					}
-				}
-			})
-			
-			.state('app.logout', {
-				url : '/logout',
-				views : {
-					'menuContent' : {
-						templateUrl : 'templates/logout.html',
-						controller : 'PlaylistCtrl'
-					}
-				}
-			})
+      .state('app.chat', {
+        url: '/chat',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/chat.html'
+          }
+        }
+      })
 
-			.state('app.single', {
-				url : '/playlists/:playlistId',
-				views : {
-					'menuContent' : {
-						templateUrl : 'templates/playlist.html',
-						controller : 'PlaylistCtrl'
-					}
-				}
-			});
-			// if none of the above states are matched, use this as the fallback
-//			$urlRouterProvider.otherwise('/app/chat');
-			
-			// this is due to the chatUI widget with hyperlinks, so if anytime other than the first login a link is clicked go to chat.
-			$urlRouterProvider.otherwise(function($injector, $location){
-				   var state = $injector.get('$state');
-				   if(state.current.name == '') {
-					   state.go('app.playlists');
-				   }
-				   else {
-					   state.go('app.chat');
-				   }
-				   return $location.path();
-				});
-			
+      .state('app.feeds', {
+        url: '/feeds',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/feeds.html',
+            controller: 'FeedsCtrl'
+          }
+        }
+      })
 
-			// Configure Auth0
-			authProvider.init({
-				domain : 'faeez.auth0.com',
-				clientID : 'Cna9qHgpmvlcoAMZMhJFPoTGbNigvrmh',
-				loginState : 'login'
-			});
+      .state('app.single', {
+        url: '/feeds/:feedId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/feed.html',
+            controller: 'FeedCtrl'
+          }
+        }
+      });
+      // if none of the above states are matched, use this as the fallback
+      //			$urlRouterProvider.otherwise('/app/chat');
 
-			jwtInterceptorProvider.tokenGetter = function(store, jwtHelper,
-					auth) {
-				var idToken = store.get('token');
-				var refreshToken = store.get('refreshToken');
-				if (!idToken || !refreshToken) {
-					return null;
-				}
-				if (jwtHelper.isTokenExpired(idToken)) {
-					return auth.refreshIdToken(refreshToken).then(
-							function(idToken) {
-								store.set('token', idToken);
-								return idToken;
-							});
-				} else {
-					return idToken;
-				}
-			}
+      // this is due to the chatUI widget with hyperlinks, so if anytime other than the first login a link is clicked go to chat.
+      $urlRouterProvider.otherwise(function($injector, $location) {
+        var state = $injector.get('$state');
+        if (state.current.name == '') {
+          state.go('app.feeds');
+        } else {
+          state.go('app.chat');
+        }
+        return $location.path();
+      });
 
-			$httpProvider.interceptors.push('jwtInterceptor');
 
-		}).run(function($rootScope, auth, store) {
-	$rootScope.$on('$locationChangeStart', function() {
-		if (!auth.isAuthenticated) {
-			var token = store.get('token');
-			if (token) {
-				auth.authenticate(store.get('profile'), token);
-			}
-		}
+    })
+  //.run(function($rootScope, auth, store) {})
 
-	});
-});
+;
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,210 +1,202 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function($scope, auth, $state, store) {
-	  function doAuth() {
-	    auth.signin({
-	      closable: false,
-	      // This asks for the refresh token
-	      // So that the user never has to log in again
-	      authParams: {
-	        scope: 'openid offline_access'
-	      }
-	    }, function(profile, idToken, accessToken, state, refreshToken) {
-	      store.set('profile', profile);
-	      store.set('token', idToken);
-	      store.set('refreshToken', refreshToken);
-	      $state.go('app.browse');
-	    }, function(error) {
-	      console.log("There was an error logging in", error);
-	    });
-	  }
+  
+  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-	  $scope.$on('$ionic.reconnectScope', function() {
-	    doAuth();
-	  });
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-	  doAuth();
-	  
-	  
-	})
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+      $scope.modal.hide();
+    };
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+    // Open the login modal
+    $scope.login = function() {
+      $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+      console.log('Doing login', $scope.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function() {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
+
+.controller('FeedsCtrl', function($scope, auth, store, $state, $timeout, PersonService, $cordovaToast) {
+  $scope.auth = auth;
+  $scope.playlists = [{
+    title: 'Reggae11',
+    id: 1
+  }, {
+    title: 'Chill',
+    id: 2
+  }, {
+    title: 'Dubstep',
+    id: 3
+  }, {
+    title: 'Indie',
+    id: 4
+  }, {
+    title: 'Rap',
+    id: 5
+  }, {
+    title: 'Cowbell',
+    id: 6
+  }];
+
+
+
+
+  $scope.items = [];
+  $scope.newItems = [];
+
+  PersonService.GetFeed().then(function(items) {
+    $scope.items = items;
   });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
+  $scope.doRefresh = function() {
+    var addedNumber = 0;
+    if ($scope.newItems.length > 0) {
+      $scope.items = $scope.newItems.concat($scope.items);
+      addedNumber = $scope.newItems.length;
+      //Stop the ion-refresher from spinning
+      $scope.$broadcast('scroll.refreshComplete');
+
+      $scope.newItems = [];
+    } else {
+      PersonService.GetNewUsers().then(function(items) {
+        $scope.items = items.concat($scope.items);
+        addedNumber = items.length;
+        //Stop the ion-refresher from spinning
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    }
+    var msg = addedNumber == 0 ? 'Nothing new yet.' : 'Loaded ' + addedNumber + ' new posts.';
+    $cordovaToast.showShortBottom(msg)
+      .then(function(success) {
+        // Do something on success
+      }, function(error) {
+        // Handle error
+      });
   };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
+  $scope.loadMore = function() {
+    PersonService.GetOldUsers().then(function(items) {
+      $scope.items = $scope.items.concat(items);
+
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
   };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
+  var CheckNewItems = function() {
     $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+      PersonService.GetNewUsers().then(function(items) {
+        $scope.newItems = items.concat($scope.newItems);
+
+        CheckNewItems();
+      });
+    }, 10000);
+  }
+
+  CheckNewItems();
+  
+  $scope.liked = function(feedId) {
+	  console.log('Liked' , feedId);
+	  
+  }
+})
+
+.controller('FeedCtrl', function($scope, auth, store, $state, $timeout, PersonService) {
+  $scope.logout = function() {
+    console.log('logout');
+    auth.signout();
+    store.remove('token');
+    store.remove('profile');
+    store.remove('refreshToken');
+    $state.go('login', {}, {
+      reload: true
+    });
   };
-})
 
-.controller('PlaylistsCtrl', function($scope,auth,store,$state,$timeout, PersonService,$cordovaToast) {
-	$scope.auth = auth;
-  $scope.playlists = [
-    { title: 'Reggae11', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-  
-  
-	  
-	  
-	  $scope.items = [];
-	  $scope.newItems = [];
-	  
-	  PersonService.GetFeed().then(function(items){
-		$scope.items = items;
-	  });
-	  
-	  $scope.doRefresh = function() {
-		  var addedNumber = 0;
-			if($scope.newItems.length > 0){
-				$scope.items = $scope.newItems.concat($scope.items);
-				addedNumber = 	$scope.newItems.length;
-				//Stop the ion-refresher from spinning
-				$scope.$broadcast('scroll.refreshComplete');
-				
-				$scope.newItems = [];
-			} else {
-				PersonService.GetNewUsers().then(function(items){
-					$scope.items = items.concat($scope.items);
-					addedNumber = items.length;
-					//Stop the ion-refresher from spinning
-					$scope.$broadcast('scroll.refreshComplete');
-				});
-			}
-			var msg = addedNumber==0 ? 'Nothing new yet.' : 'Loaded ' + addedNumber + ' new posts.';
-			$cordovaToast.showShortBottom(msg)
-	        .then(function(success) {
-	            // Do something on success
-	        }, function(error) {
-	            // Handle error
-	        });
-	  };
-	  
-	  $scope.loadMore = function(){
-	    PersonService.GetOldUsers().then(function(items) {
-	      $scope.items = $scope.items.concat(items);
-		  
-	      $scope.$broadcast('scroll.infiniteScrollComplete');
-	    });
-	  };
-	  
-	   var CheckNewItems = function(){
-			$timeout(function(){
-				PersonService.GetNewUsers().then(function(items){
-					$scope.newItems = items.concat($scope.newItems);
-				
-					CheckNewItems();
-				});
-			},10000);
-	   }
-	  
-	  CheckNewItems();
-})
-
-.controller('PlaylistCtrl', function($scope,auth,store,$state,$timeout, PersonService) {
-	$scope.logout = function() {
-		  console.log('logout');
-		    auth.signout();
-		    store.remove('token');
-		    store.remove('profile');
-		    store.remove('refreshToken');
-		    $state.go('login', {}, {reload: true});
-		  };
-	
 })
 
 
-.controller('chatController',function($scope,auth,$state,$window,$firebaseAuth){
-	
-		var chatRef = new Firebase('https://daughertyapp.firebaseio.com/');
-		var auth = $firebaseAuth(chatRef);
-		$scope.msg = "Initializing chat..";
-		$scope.date = new Date();
-		
-		$scope.login = function(socialPlatform) {
-			$scope.loginProgress = true;
-			auth.$authWithOAuthPopup(socialPlatform).then(function(authData) {
-			    console.log("Logged in as:", authData.uid);
-			    $scope.loggedIn = true;
-				$scope.loginProgress = false;
-			  }).catch(function(error) {
-				  console.log("Authentication failed:", error);
-			      $scope.loginProgress = false;
-			      console.log(error);
-			      $scope.msg = "";
-			      $( "#firechat-wrapper" ).effect( "shake",{times:4},1000 );
-			  });
-		}
-		
-		$scope.logout = function() {
-			chatRef.unauth();
-			$scope.msg = "Signing out of chat..";
-			$scope.loggedIn = false;
-			$scope.loginProgress = true;
-			$state.go('app.playlists'); // adding this for device.. location.href doesnt work on device
-			$window.location.reload();
-			
-		}
-		auth.$onAuth(function(authData) {
-		  // Once authenticated, instantiate Firechat with our user id and user name
-		  if (authData) {
-			  $scope.loginProgress = false;
-			  $scope.loggedIn = true;
-			  if(authData.provider == 'facebook') {
-				  $scope.userName = authData.facebook.displayName;
-				  $scope.userImg = authData.facebook.profileImageURL;
-				  $scope.userEmail = authData.facebook.email; // Email works only if user has exposed.
-			  }
-			  if(authData.provider == 'twitter') {
-				  $scope.userName = authData.twitter.displayName;
-				  $scope.userImg = authData.twitter.profileImageURL;
-				  $scope.userEmail = authData.twitter.email;
-			  }
-			  if(authData.provider == 'google') {
-				  $scope.userName = authData.google.displayName;
-				  $scope.userImg = authData.google.profileImageURL;
-				  $scope.userEmail = authData.google.email;
-			  }
-			  var chat = new FirechatUI(chatRef, angular.element(document.querySelector('#firechat-wrapper')));
-			  chat.setUser(authData.uid, authData[authData.provider].displayName);
-		  }
-		});
-		
+.controller('chatController', function($scope, auth, $state, $window, $firebaseAuth) {
+
+  var chatRef = new Firebase('https://daughertyapp.firebaseio.com/');
+  var auth = $firebaseAuth(chatRef);
+  $scope.msg = "Initializing chat..";
+  $scope.date = new Date();
+
+  $scope.login = function(socialPlatform) {
+    $scope.loginProgress = true;
+    auth.$authWithOAuthPopup(socialPlatform).then(function(authData) {
+      console.log("Logged in as:", authData.uid);
+      $scope.loggedIn = true;
+      $scope.loginProgress = false;
+    }).catch(function(error) {
+      console.log("Authentication failed:", error);
+      $scope.loginProgress = false;
+      console.log(error);
+      $scope.msg = "";
+      $("#firechat-wrapper").effect("shake", {
+        times: 4
+      }, 1000);
+    });
+  }
+
+  $scope.logout = function() {
+    chatRef.unauth();
+    $scope.msg = "Signing out of chat..";
+    $scope.loggedIn = false;
+    $scope.loginProgress = true;
+    $state.go('app.playlists'); // adding this for device.. location.href doesnt work on device
+    $window.location.reload();
+
+  }
+  auth.$onAuth(function(authData) {
+    // Once authenticated, instantiate Firechat with our user id and user name
+    if (authData) {
+      $scope.loginProgress = false;
+      $scope.loggedIn = true;
+      if (authData.provider == 'facebook') {
+        $scope.userName = authData.facebook.displayName;
+        $scope.userImg = authData.facebook.profileImageURL;
+        $scope.userEmail = authData.facebook.email; // Email works only if user has exposed.
+      }
+      if (authData.provider == 'twitter') {
+        $scope.userName = authData.twitter.displayName;
+        $scope.userImg = authData.twitter.profileImageURL;
+        $scope.userEmail = authData.twitter.email;
+      }
+      if (authData.provider == 'google') {
+        $scope.userName = authData.google.displayName;
+        $scope.userImg = authData.google.profileImageURL;
+        $scope.userEmail = authData.google.email;
+      }
+      var chat = new FirechatUI(chatRef, angular.element(document.querySelector('#firechat-wrapper')));
+      chat.setUser(authData.uid, authData[authData.provider].displayName);
+    }
+  });
+
 });
-
-
